48a8276077649a12564e1c73e92d8db40bf45b30
z43460018724 <2351655638@qq.com>
2020-12-02 19:21:35 +0800

    创建springboot单体项目

---
+++ src/main/java/com/zhao/springbootsingle/SpringbootsingleApplication.java
@@ -0,0 +1,13 @@
+package com.zhao.springbootsingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsingleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootsingleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhao/springbootsingle/SpringbootsingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhao.springbootsingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsingleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
